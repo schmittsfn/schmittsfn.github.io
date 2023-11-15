b3331696cc52b783fe4eefead1d85df95a8bd34f
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2023-11-15 22:16:00 +0000

    feat: Change tableView blog post title [ci skip]

--- _projects/ph1lm.md
+++
@@ -1,9 +0,0 @@
----
-layout: page
-title: ph1lm
-description: Technology and the film industry; a study
-img: assets/img/banner_ph1lm.jpg
-importance: 3
-category: personal projects
-redirect: https://ph1lm.com
----
--- assets/js/dark_mode.js
+++ assets/js/dark_mode.js
@@ -1,9 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const mode_toggle = document.getElementById("light-toggle");
-
-    mode_toggle.addEventListener("click", function() {
-        toggleTheme(localStorage.getItem("theme"));
-        location.reload();
-    });
-});
-
+document.addEventListener("DOMContentLoaded",function(){document.getElementById("light-toggle").addEventListener("click",function(){toggleTheme(localStorage.getItem("theme")),location.reload()})});
